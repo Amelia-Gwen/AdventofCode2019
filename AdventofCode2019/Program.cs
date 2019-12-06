3c659d23c24263300c576c4fae79982709d1b8cb
bruglesco <klitzinger@gmail.com>
2019-12-06 11:53:04 -0500

    Clean slate for Day 4

--- AdventofCode2019/Program.cs
+++ AdventofCode2019/Program.cs
@@ -13,19 +13,13 @@ namespace AdventofCode2019
                  @"C:\Developer\Projects\AdventOfCode\AdventofCode2019\AdventofCode2019\Day3Input.txt";
             ProcessInput processor = new ProcessInput();
             GenerateAnswers generator = new GenerateAnswers();
-            long answer = 0;
-
+            
             if (File.Exists(inputFile))
             {
-                string[] wires = File.ReadAllLines(inputFile);
-                List<string> one = processor.GenerateTokens(wires[0]);
-                List<string> two = processor.GenerateTokens(wires[1]);
-                List<Point> wireOne = processor.GenerateListOfPoints(one);
-                List<Point> wireTwo = processor.GenerateListOfPoints(two);
-                answer = generator.GenerateAnswerDayThreePartTwo(wireOne, wireTwo);
+
             }
 
-            Console.WriteLine(answer);
+            Console.WriteLine();
             Console.ReadKey();
         }
     }
