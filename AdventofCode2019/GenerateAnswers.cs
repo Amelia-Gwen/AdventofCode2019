eb1d4a933169117476f9f22711b7b972eaa6bb9c
bruglesco <klitzinger@gmail.com>
2019-12-06 13:17:59 -0500

    Day 4 Part 2 solved

--- AdventofCode2019/GenerateAnswers.cs
+++ AdventofCode2019/GenerateAnswers.cs
@@ -134,8 +134,12 @@ namespace AdventofCode2019
             {
                 hasAdjacentMatch = false;
                 string password = i.ToString();
-                if (password[0] == password[1] || password[1] == password[2] || password[2] == password[3] ||
-                    password[3] == password[4] || password[4] == password[5])
+                
+                if (password[0] == password[1] && password[1] != password[2]
+                    || password[0] != password[1] && password[1] == password[2] && password[2] != password[3]
+                    || password[1] != password[2] && password[2] == password[3] && password[3] != password[4]
+                    || password[2] != password[3] && password[3] == password[4] && password[4] != password[5]
+                    || password[3] != password[4] && password[4] == password[5])
                 {
                     hasAdjacentMatch = true;
                 }
